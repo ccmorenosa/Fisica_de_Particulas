512ea051ca73cc2d1669f463caea63b494c4745e
afmorenosa <afmorenosa@unal.edu.co>
2020-10-05 02:47:12 -0500

    Complete excercise 2

--- ex_2/ex_2.C
+++ ex_2/ex_2.C
@@ -33,6 +33,16 @@ void ex_2 () {
   vector<float> *track_R4_phi = {};
   vector<float> *track_R4_m = {};
 
+  vector<float> *reco_R10_pt = {};
+  vector<float> *reco_R10_trimmed_pt = {};
+  vector<float> *reco_R10_m = {};
+  vector<float> *reco_R10_trimmed_m = {};
+
+  vector<float> *truth_R10_pt = {};
+  vector<float> *truth_R10_trimmed_pt = {};
+  vector<float> *truth_R10_m = {};
+  vector<float> *truth_R10_trimmed_m = {};
+
 
   // Get the branches address into the variables
   tree->SetBranchAddress("NPV", &npv);
@@ -57,6 +67,16 @@ void ex_2 () {
   tree->SetBranchAddress("TrackJets_R4_eta", &track_R4_eta);
   tree->SetBranchAddress("TrackJets_R4_phi", &track_R4_phi);
   tree->SetBranchAddress("TrackJets_R4_m", &track_R4_m);
+
+  tree->SetBranchAddress("RecoJets_R10_pt", &reco_R10_pt);
+  tree->SetBranchAddress("RecoJets_R10_Trimmed_pt", &reco_R10_trimmed_pt);
+  tree->SetBranchAddress("RecoJets_R10_m", &reco_R10_m);
+  tree->SetBranchAddress("RecoJets_R10_Trimmed_m", &reco_R10_trimmed_m);
+
+  tree->SetBranchAddress("TruthJets_R10_pt", &truth_R10_pt);
+  tree->SetBranchAddress("TruthJets_R10_Trimmed_pt", &truth_R10_trimmed_pt);
+  tree->SetBranchAddress("TruthJets_R10_m", &truth_R10_m);
+  tree->SetBranchAddress("TruthJets_R10_Trimmed_m", &truth_R10_trimmed_m);
 
   // Create the canvas to plot the histograms
   TCanvas *canvas = new TCanvas(
@@ -458,6 +478,10 @@ void ex_2 () {
   canvas->Print("lead_track_reco_cut.svg");
   canvas->Clear();
 
+  //--------------------------------------------------------------------------//
+  //                                  DeltaR                                  //
+  //--------------------------------------------------------------------------//
+
   TH1F *hist_DR_reco_truth = new TH1F(
     "Delta R reco",
     "Delta R; #Delta R; Events",
@@ -536,7 +560,6 @@ void ex_2 () {
   }
 
   std::cout << "Done DeltaR" << std::endl;
-  // canvas->SetLogy(0);
 
   hist_DR_reco_truth->SetMarkerStyle(20);
   hist_DR_reco_truth->SetMarkerColor(kRed);
@@ -554,4 +577,301 @@ void ex_2 () {
   hist_DR_track_truth->DrawNormalized("Same");
   canvas->Print("DeltaR.svg");
   canvas->Clear();
+
+  //--------------------------------------------------------------------------//
+  //                               Matched Jets                               //
+  //--------------------------------------------------------------------------//
+
+  TH1F *hist_matched_reco_truth_20 = new TH1F(
+    "Matched Delta R [reco] > 20 GeV",
+    "Matched Delta R; pTjet/pTtruth; Events",
+    20,
+    0,
+    5
+  );
+
+  TH1F *hist_matched_reco_truth_100 = new TH1F(
+    "Matched Delta R [reco] > 100 GeV",
+    "Matched Delta R; pTjet/pTtruth; Events",
+    20,
+    0,
+    5
+  );
+
+  TH1F *hist_matched_reco_truth_500 = new TH1F(
+    "Matched Delta R [reco] > 500 GeV",
+    "Matched Delta R; pTjet/pTtruth; Events",
+    20,
+    0,
+    5
+  );
+
+  TH1F *hist_matched_track_truth_20 = new TH1F(
+    "Matched Delta R [track] > 20 GeV",
+    "Matched Delta R; pTjet/pTtruth; Events",
+    20,
+    0,
+    5
+  );
+
+  TH1F *hist_matched_track_truth_100 = new TH1F(
+    "Matched Delta R [track] > 100 GeV",
+    "Matched Delta R; pTjet/pTtruth; Events",
+    20,
+    0,
+    5
+  );
+
+  TH1F *hist_matched_track_truth_500 = new TH1F(
+    "Matched Delta R [track] > 500 GeV",
+    "Matched Delta R; pTjet/pTtruth; Events",
+    20,
+    0,
+    5
+  );
+
+  for (i = 0; i < nentries; i++) {
+    nbytes = tree->GetEntry(i);
+
+    if(truth_R4_pt->size()!=0){
+      TLorentzVector truthJet;
+      truthJet.SetPtEtaPhiM(
+        truth_R4_pt->at(0),
+        truth_R4_eta->at(0),
+        truth_R4_phi->at(0),
+        truth_R4_m->at(0)
+      );
+
+      if(reco_R4_pt->size() != 0){
+        TLorentzVector recoJet;
+        recoJet.SetPtEtaPhiM(
+          reco_R4_pt->at(0),
+          reco_R4_eta->at(0),
+          reco_R4_phi->at(0),
+          reco_R4_m->at(0)
+        );
+
+        if (truthJet.DeltaR(recoJet) < 0.3) {
+          if (truth_R4_pt->at(0) > 20000.) {
+            hist_matched_reco_truth_20->Fill(
+              truth_R4_pt->at(0) / reco_R4_pt->at(0),
+              evtw
+            );
+          }
+
+          if (truth_R4_pt->at(0) > 100000.) {
+            hist_matched_reco_truth_100->Fill(
+              truth_R4_pt->at(0) / reco_R4_pt->at(0),
+              evtw
+            );
+          }
+
+          if (truth_R4_pt->at(0) > 500000.) {
+            hist_matched_reco_truth_500->Fill(
+              truth_R4_pt->at(0) / reco_R4_pt->at(0),
+              evtw
+            );
+          }
+        }
+      }
+
+
+      if(track_R4_pt->size() != 0){
+        TLorentzVector trackJet;
+        trackJet.SetPtEtaPhiM(
+          track_R4_pt->at(0),
+          track_R4_eta->at(0),
+          track_R4_phi->at(0),
+          track_R4_m->at(0)
+        );
+
+        //Plot the Delta R
+        if (truthJet.DeltaR(trackJet) < 0.3) {
+          if (truth_R4_pt->at(0)>20000.) {
+            hist_matched_track_truth_20->Fill(
+              truth_R4_pt->at(0) / track_R4_pt->at(0),
+              evtw
+            );
+          }
+
+          if (truth_R4_pt->at(0)>100000.) {
+            hist_matched_track_truth_100->Fill(
+              truth_R4_pt->at(0) / track_R4_pt->at(0),
+              evtw
+            );
+          }
+
+          if (truth_R4_pt->at(0)>500000.) {
+            hist_matched_track_truth_500->Fill(
+              truth_R4_pt->at(0) / track_R4_pt->at(0),
+              evtw
+            );
+          }
+        }
+      }
+
+    }
+  }
+
+  std::cout << "Done Matched Jets" << std::endl;
+
+  hist_matched_reco_truth_20->SetMarkerStyle(20);
+  hist_matched_reco_truth_20->SetMarkerColor(kRed);
+  hist_matched_reco_truth_20->Draw("");
+
+  hist_matched_reco_truth_100->SetMarkerStyle(21);
+  hist_matched_reco_truth_100->SetMarkerColor(kBlue);
+  hist_matched_reco_truth_100->Draw("Same");
+
+  hist_matched_reco_truth_500->SetMarkerStyle(22);
+  hist_matched_reco_truth_500->SetMarkerColor(kOrange);
+  hist_matched_reco_truth_500->Draw("Same");
+  canvas->Print("Matched_reco.svg");
+  canvas->Clear();
+
+  hist_matched_track_truth_20->SetMarkerStyle(20);
+  hist_matched_track_truth_20->SetMarkerColor(kRed);
+  hist_matched_track_truth_20->Draw("");
+
+  hist_matched_track_truth_100->SetMarkerStyle(21);
+  hist_matched_track_truth_100->SetMarkerColor(kBlue);
+  hist_matched_track_truth_100->Draw("Same");
+
+  hist_matched_track_truth_500->SetMarkerStyle(22);
+  hist_matched_track_truth_500->SetMarkerColor(kOrange);
+  hist_matched_track_truth_500->Draw("Same");
+  canvas->Print("Matched_track.svg");
+  canvas->Clear();
+
+  //--------------------------------------------------------------------------//
+  //                                Large Jets                                //
+  //--------------------------------------------------------------------------//
+
+  TH1F *hist_reco_R10_pt = new TH1F(
+    "Leading jet reco R10 pT",
+    "Leading jet reco R10 pT R10; pT(GeV); Events",
+    20,
+    0,
+    100
+  );
+
+  TH1F *hist_reco_R10_trimmed_pt = new TH1F(
+    "Leading jet reco R10 pT [Trimmed]",
+    "Leading jet reco R10 pT R10; pT(GeV); Events",
+    20,
+    0,
+    100
+  );
+
+  TH1F *hist_truth_R10_pt = new TH1F(
+    "Leading jet truth R10 pT",
+    "Leading jet truth R10 pT; pT(GeV); Events",
+    20,
+    0,
+    100
+  );
+
+  TH1F *hist_truth_R10_trimmed_pt = new TH1F(
+    "Leading jet truth R10 pT [Trimmed]",
+    "Leading jet truth R10 pT; pT(GeV); Events",
+    20,
+    0,
+    100
+  );
+
+  TH1F *hist_reco_R10_m = new TH1F(
+    "Leading jet reco R10 mass",
+    "Leading jet reco R10 mass; Mass; Events",
+    20,
+    0,
+    100
+  );
+
+  TH1F *hist_reco_R10_trimmed_m = new TH1F(
+    "Leading jet reco R10 mass [Trimmed]",
+    "Leading jet reco R10 mass; Mass; Events",
+    20,
+    0,
+    100
+  );
+
+  TH1F *hist_truth_R10_m = new TH1F(
+    "Leading jet truth R10 mass",
+    "Leading jet truth R10 mass; Mass; Events",
+    20,
+    0,
+    100
+  );
+
+  TH1F *hist_truth_R10_trimmed_m = new TH1F(
+    "Leading jet truth R10 mass [Trimmed]",
+    "Leading jet truth R10 mass; Mass; Events",
+    20,
+    0,
+    100
+  );
+
+  for (i = 0; i < nentries; i++) {
+    nbytes = tree->GetEntry(i);
+
+    if(reco_R10_pt->size() > 0){
+      hist_reco_R10_pt->Fill(reco_R10_pt->at(0)/1000.,evtw);
+      hist_reco_R10_trimmed_pt->Fill(reco_R10_trimmed_pt->at(0)/1000., evtw);
+
+      hist_reco_R10_m->Fill(reco_R10_m->at(0)/1000., evtw);
+      hist_reco_R10_trimmed_m->Fill(reco_R10_trimmed_m->at(0)/1000., evtw);
+    }
+
+    if (truth_R10_m->size() > 0) {
+      hist_truth_R10_pt->Fill(truth_R10_pt->at(0)/1000.,evtw);
+      hist_truth_R10_trimmed_pt->Fill(truth_R10_trimmed_pt->at(0)/1000., evtw);
+
+      hist_truth_R10_m->Fill(truth_R10_m->at(0)/1000.);
+      hist_truth_R10_trimmed_m->Fill(truth_R10_trimmed_m->at(0)/1000., evtw);
+    }
+  }
+
+  std::cout << "Done Large Jets" << std::endl;
+
+
+  // pT plots
+  hist_reco_R10_pt->SetMarkerStyle(20);
+  hist_reco_R10_pt->SetMarkerColor(kRed);
+  hist_reco_R10_pt->Draw("");
+
+  hist_reco_R10_trimmed_pt->SetMarkerStyle(21);
+  hist_reco_R10_trimmed_pt->SetMarkerColor(kBlue);
+  hist_reco_R10_trimmed_pt->Draw("Same");
+  canvas->Print("reco_R10_pt.svg");
+  canvas->Clear();
+
+  hist_truth_R10_pt->SetMarkerStyle(20);
+  hist_truth_R10_pt->SetMarkerColor(kRed);
+  hist_truth_R10_pt->Draw("");
+
+  hist_truth_R10_trimmed_pt->SetMarkerStyle(21);
+  hist_truth_R10_trimmed_pt->SetMarkerColor(kBlue);
+  hist_truth_R10_trimmed_pt->Draw("Same");
+  canvas->Print("truth_R10_pt.svg");
+  canvas->Clear();
+
+  // Mass plots
+  hist_reco_R10_m->SetMarkerStyle(20);
+  hist_reco_R10_m->SetMarkerColor(kRed);
+  hist_reco_R10_m->Draw("");
+
+  hist_reco_R10_trimmed_m->SetMarkerStyle(21);
+  hist_reco_R10_trimmed_m->SetMarkerColor(kBlue);
+  hist_reco_R10_trimmed_m->Draw("Same");
+  canvas->Print("reco_R10_m.svg");
+  canvas->Clear();
+
+  hist_truth_R10_m->SetMarkerStyle(20);
+  hist_truth_R10_m->SetMarkerColor(kRed);
+  hist_truth_R10_m->Draw("");
+
+  hist_truth_R10_trimmed_m->SetMarkerStyle(21);
+  hist_truth_R10_trimmed_m->SetMarkerColor(kBlue);
+  hist_truth_R10_trimmed_m->Draw("Same");
+  canvas->Print("truth_R10_m.svg");
 }
